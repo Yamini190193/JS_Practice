7b101474944438caa1d14393696432c9e1f1d106
Yamini190193 <42149160+Yamini190193@users.noreply.github.com>
2023-12-12 19:29:51 +0000

    datatypes

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553;
+let accountEmail = "abc@gmail.com";
+var accountPassword = "12345";
+accountCity = "Gujrat";
+
+let accountState;
+
+ // accountId =  2;  // not allowed
+
+ accountEmail="hc@hc.com";
+ accountPassword="212121121";
+ accountCity="MH";
+
+ console.table([accountId, accountEmail, accountPassword, accountCity,accountState])
---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+//number
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => value is not assigned 
+//symbol => unique
+
+//object
+
+console.log(typeof "hitesh"); //string
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
+
