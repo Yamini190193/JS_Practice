d2c3d5134084bf8c155cd3ead60ff468541c6b6f
Yamini190193 <42149160+Yamini190193@users.noreply.github.com>
2023-12-13 20:00:08 +0000

    array in JS

---
+++ .devcontainer/02_basics/02_array.js
@@ -0,0 +1,36 @@
+const marvel_heros = ['thor', 'Ironman', 'spiderman'];
+
+const dc_heros = ['suparman', 'flash', 'batman'];
+
+// console.log(marvel_heros.push(dc_heros));
+
+// console.log(marvel_heros);
+
+// console.log(marvel_heros[4]);
+
+// console.log(marvel_heros[3][0]);
+
+// const all_heros = marvel_heros.concat(dc_heros);
+// console.log(all_heros);
+
+
+// const all_new_heros =[...marvel_heros, ...dc_heros]
+
+// console.log(all_new_heros);
+
+// const nestedArray = [1,2,3,[4,5,6],7,[6,7,[4,5]]];
+
+// const realArray = nestedArray.flat(Infinity);
+// console.log(realArray);
+
+console.log(Array.isArray("yamini"));
+
+console.log(Array.from("yamini"));
+
+console.log(Array.from({name: "yamini"}));
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1,score2,score3));
