3479786356113f67a03de73d4b87c7583d52128d
Yamini190193 <42149160+Yamini190193@users.noreply.github.com>
2023-12-14 22:09:20 +0000

    for and for-each loop

---
+++ .devcontainer/04_control_flow/one.js
@@ -0,0 +1,25 @@
+// // for loop
+// const numbers = [2 , 4, 6, 8, 10 ];
+
+// for( i = 0 ; i < numbers.length; i++){
+//     console.log(numbers[i]);
+// }
+
+
+//  //for each
+// const numbers2 = [4, 6, 8, 10, 12];
+// numbers2.forEach((number) =>{
+//     console.log(number * 2);
+// })
+
+
+let a = 2;
+let b = "2";
+
+// if(a == b){
+//     console.log("both are same");
+// }
+
+if(a === b){
+    console.log("both are same");
+}
