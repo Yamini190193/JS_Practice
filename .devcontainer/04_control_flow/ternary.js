4d2d5718648670b112c1b35189506a602976f26c
Yamini190193 <42149160+Yamini190193@users.noreply.github.com>
2023-12-16 02:42:29 +0000

    ternary operator

---
+++ .devcontainer/04_control_flow/ternary.js
@@ -0,0 +1,11 @@
+// Conditional (ternary) operator
+
+//The conditional (ternary) operator is the only JavaScript operator that takes three operands: 
+//a condition followed by a question mark (?), then an expression to execute if the condition is truthy followed by a colon (:), 
+//and finally the expression to execute if the condition is falsy. 
+//This operator is frequently used as an alternative to an if...else statement.
+
+
+const age = 26;
+const beverage = age >= 21 ? "Beer" : "Juice";
+console.log(beverage);  // "Beer"
