3d3f645cffb1cac7f2964c9ba574358da87c91bb
Yamini190193 <42149160+Yamini190193@users.noreply.github.com>
2023-12-17 13:11:30 +0000

    while , do-while loop

---
+++ .devcontainer/05_iterations/two.js
@@ -0,0 +1,21 @@
+
+let i = 0;
+while (i<=10) {
+    console.log(i);
+    i = i + 2;
+}
+
+const arr = ["aarya", "hitesh", "yamini", "naman", "mohini"];
+let j = 0;
+while (j < arr.length) {
+
+    console.log(arr[j]);
+    j = j + 1;
+    
+}
+
+let k = 11;
+do {
+    console.log("k:" ,k);
+    k++;
+} while (k <= 10);
